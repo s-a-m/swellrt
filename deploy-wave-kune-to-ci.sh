92f6c405713fb5ed73f95f05ab2a28a21e9baf69
Vicente J. Ruiz Jurado <vjrj@ourproject.org>
2013-11-24 17:53:16 +0100

    Deploy to archiva scripts

---
+++ deploy-wave-kune-to-ci.sh
@@ -0,0 +1,9 @@
+VERSION=0.3.43
+ant dist-api dist-libraries dist-proto dist-pst dist-robot-client-api dist-pst-dep dist-pst dist-server-dep dist-server 
+for i in `cat kune-artifacts-alone.txt` 
+do 
+        cd dist
+  mvn deploy:deploy-file -DgroupId=org.waveprotocol -DartifactId=$i -Dversion=$VERSION -Dfile=$i.jar -Dpackaging=jar -Durl=http://archiva.comunes.org/repository/comunes-internal/ -DrepositoryId=comunes-internal
+  cd ..
+done
+
