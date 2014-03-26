9b783b2fe636ed9d625a7a05ca175a320c0f1335
vjrj <vjrj@ourproject.org>
2014-03-26 11:50:45 +0100

    New kune version

--- deploy-wave-kune-to-ci.sh
+++ deploy-wave-kune-to-ci.sh
@@ -1,6 +1,12 @@
-VERSION=0.3.44
+VERSION=0.3.46
+if [[ $# -gt 0 ]]
+then
+  ARTI=$*
+else 
+  ARTI=`cat kune-artifacts-alone.txt`
+fi
 ant dist-api dist-libraries dist-proto dist-pst dist-robot-client-api dist-pst-dep dist-pst dist-server-dep dist-server 
-for i in `cat kune-artifacts-alone.txt` 
+for i in $ARTI
 do 
         cd dist
   mvn deploy:deploy-file -DgroupId=org.waveprotocol -DartifactId=$i -Dversion=$VERSION -Dfile=$i.jar -Dpackaging=jar -Durl=http://archiva.comunes.org/repository/comunes-internal/ -DrepositoryId=comunes-internal
