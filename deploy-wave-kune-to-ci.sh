1429b4f264281d8c616aab3ade9d37a94ef95dba
Antonio Tapiador del Dujo <atapiador@ucm.es>
2014-03-27 12:15:23 +0100

    Configure artifacts deployment

--- deploy-wave-kune-to-ci.sh
+++ deploy-wave-kune-to-ci.sh
@@ -4,9 +4,9 @@ VERSION=0.1.0-p2pv-SNAPSHOT
 # Left URL and REPOID empty for local repo installation
 
 #URL=http://archiva.comunes.org/repository/comunes-internal/
-URL=
+URL=http://grasia.fdi.ucm.es/p2pvalue/artifactory/p2pvalue-snapshot/
 #REPOID=comunes-internal
-REPOID=
+REPOID=p2pvalue-snapshot
 
 #ant dist-api dist-libraries dist-proto dist-pst dist-robot-client-api dist-pst-dep dist-pst dist-server-dep dist-server 
 for i in `cat kune-artifacts-alone.txt` 
